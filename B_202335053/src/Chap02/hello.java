6e7a563e9798e2f1f9459113505c872c989b7552
노정운 <nojeong-un@nojeong-un-ui-MacBookAir.local>
2023-09-12 22:02:46 +0900

    2주차 완료

---
+++ B_202335053/src/Chap02/hello.java
@@ -0,0 +1,17 @@
+package Chap02;
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int distance;
+		double shape;
+		distance = 1224000;
+		shape = 314.0;
+		
+		System.out.println("소리가 1시간 동안 가는 거리 : "+ distance + "m");
+		System.out.println("반지름이 10.0인 원의 넓이 : "+ shape);
+		
+	}
+	
+}
---
+++ B_202335053/src/Chap3/ConverToLowerToUpper.java
@@ -0,0 +1,19 @@
+package Chap3;
+
+public class ConverToLowerToUpper {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char c = 'a'; // 여기에 원하는 문자를 입력하세
+		
+		if(c >= 'a' && c <= 'z') {
+			char uppercase = (char)(c - ('a' - 'A'));
+			System.out.println("변환 전 : " + c);
+			System.out.println("변환 후 : " + uppercase);
+		} else {
+			System.out.println("입력된 문자가 소문자가 아닙니");
+		}
+	
+	}
+
+}
